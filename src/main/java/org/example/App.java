c5e6f61909e768e429dec099bc5ae31177fdf967
Aryan Sapra <aryansaprajiit@gmail.com>
2024-04-27 18:21:16 +0530

    wrote code for insert queries using jdbcTemplate

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.example.dao.StudentDao;
+import org.example.dao.StudentDaoImpl;
+import org.example.entity.Student;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -14,7 +17,19 @@ public class App
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
         JdbcTemplate jdbcTemplate = context.getBean("jdbcTemplate",JdbcTemplate.class);
-        System.out.println("printing jdbc");
-        System.out.println(jdbcTemplate.toString());
+        StudentDao studentDao = context.getBean("studentDaoImpl",StudentDaoImpl.class);
+        System.out.println("application started");
+
+
+        //writing insert query
+        /*
+           One way to write query but this is not industry standard
+        String insertQuery1 = "insert into Student values(?,?,?)";
+        int result = jdbcTemplate.update(insertQuery1,2,"Kartik","Bangalore");
+        */
+        Student student = new Student(4,"raman","Noida");
+        int result = studentDao.insert(student);
+        System.out.println("Number of rows updated... " + result);
+
     }
 }
